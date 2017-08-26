820052dd5d48732240464e37751b8dc9bafedcb9
Vicente Monfort <MAC@MacBook-de-Vicente-2.local>
2017-08-26 19:04:14 +0200

    Primera versión

---
+++ mensaje.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>ejercicio 4.1</title>
+	</head>
+	<body>
+		<?php
+		$nombre = "Vicente Monfort"
+		$edad = " 40 años"
+		echo $nombre;
+		echo $edad
+		?>
+	</body>
+</html>
